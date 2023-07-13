16108dc03aa763d3481cd896dc65e9d4f3002cc6
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2023-07-12 23:13:16 -0400

    Fixed Binding/Example usage

--- src/StarterPlayer/StarterPlayerScripts/Client/Client/BindTest.lua
+++ src/StarterPlayer/StarterPlayerScripts/Client/Client/BindTest.lua
@@ -7,13 +7,46 @@ local BindTest = {}
 function BindTest.Init()
 	local Directive = {
 		Run = function(state)
-			print(state)
+			if state == Enum.UserInputState.End then
+				print("Requested to stop running!")
+			end
+		end,
+		Dash = function(state)
+			if state == Enum.UserInputState.Begin then
+				print("Request to dash!")
+			end
+		end,
+		Zoom = function(state)
+			if state == Enum.UserInputState.Change then
+				print("Zoom is changing currently...")
+			end
 		end,
 	}
 
 	Binding.map({
 		[Enum.KeyCode.LeftShift] = Directive.Run,
+		[Enum.KeyCode.LeftControl] = Directive.Dash,
+		[Enum.UserInputType.MouseWheel] = Directive.Zoom,
 	})
+
+	--// Change existing item?
+
+	Binding.hardMap({
+		[Enum.KeyCode.LeftShift] = Binding._nil,
+	})
+	--// Add non existing item. Wont work if the item does not exist.
+
+	Binding.softMap({
+		[Enum.KeyCode.LeftShift] = function(state)
+			print("I tried to overwrite")
+		end,
+	}) --// Will not work because the value exist. Only adds something if it doesnt exist.
+
+	Binding.crossMap({
+		[Enum.KeyCode.LeftControl] = function(state)
+			print("I edited this!")
+		end,
+	}) --// Rewrite data without deleting it. Adding, editing, or removing it...
 end
 
 return BindTest
--- src/StarterPlayer/StarterPlayerScripts/Client/Main/Binding.lua
+++ src/StarterPlayer/StarterPlayerScripts/Client/Main/Binding.lua
@@ -1,10 +1,11 @@
 local UIS = game:GetService("UserInputService")
 
 type InputData = Enum.KeyCode | Enum.UserInputType
-type MappingData = { [InputData]: (InputState: Enum.UserInputState) -> () }
+type MappingData = { [InputData]: (InputState: Enum.UserInputState) -> () | nil }
 
 local BindMap = {}
 BindMap._map = nil
+BindMap._nil = function() end
 
 local function _RunMap(index: InputData, ...)
 	local Callback = BindMap._map[index]
@@ -14,7 +15,7 @@ local function _RunMap(index: InputData, ...)
 end
 
 local function ForPairs(t, callback)
-	for index, value in pairs(callback) do
+	for index, value in pairs(t) do
 		callback(index, value)
 	end
 end
@@ -30,30 +31,37 @@ function BindMap.map(NewMap: MappingData) --// Overwrite all data
 	BindMap._map = NewMap
 end
 
-function BindMap.softMap(NewMap: MappingData) --// Add those which do not exist
+function BindMap.softMap(NewMap) --// Add those which do not exist
+	print(NewMap)
 	if BindMap._map == nil then
 		BindMap._map = NewMap
 		return
 	end
 	ForPairs(NewMap, function(input, callback)
 		if BindMap._map[input] == nil then
-			BindMap._map[input] = callback
+			BindMap._map[input] = callback or nil
 		end
 	end)
 end
 
-function BindMap.hardMap(NewMap: MappingData) --// Override those which exist already.
+function BindMap.hardMap(NewMap) --// Override those which exist already.
 	if BindMap._map == nil then
 		return
 	end
 	ForPairs(NewMap, function(input, callback)
 		if BindMap._map[input] ~= nil then
-			BindMap._map[input] = callback
+			BindMap._map[input] = callback or nil
 		end
 	end)
 end
 
+function BindMap.crossMap(NewMap: MappingData) --// Will overwrite existing variables without removing entire data
+	BindMap.hardMap(NewMap)
+	BindMap.softMap(NewMap)
+end
+
 UIS.InputBegan:Connect(OnInput)
 UIS.InputEnded:Connect(OnInput)
+UIS.InputChanged:Connect(OnInput)
 
 return BindMap
