920d82532c83318c2b0533f511591fc414b7f0b3
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2023-07-13 20:44:04 -0400

    New Binds

--- src/StarterPlayer/StarterPlayerScripts/Client/Main/Binding.lua
+++ src/StarterPlayer/StarterPlayerScripts/Client/Main/Binding.lua
@@ -1,18 +1,6 @@
 local UIS = game:GetService("UserInputService")
 
-type InputData = Enum.KeyCode | Enum.UserInputType
-type MappingData = { [InputData]: (InputState: Enum.UserInputState) -> () | nil }
-
-local BindMap = {}
-BindMap._map = nil
-BindMap._nil = function() end
-
-local function _RunMap(index: InputData, ...)
-	local Callback = BindMap._map[index]
-	if Callback then
-		Callback(...)
-	end
-end
+type Input = Enum.UserInputType | Enum.KeyCode
 
 local function ForPairs(t, callback)
 	for index, value in pairs(t) do
@@ -20,48 +8,50 @@ local function ForPairs(t, callback)
 	end
 end
 
-local function OnInput(input, gpe)
-	if not gpe then
-		local CaughtInput = (input.KeyCode == Enum.KeyCode.Unknown) and input.UserInputType or input.KeyCode
-		_RunMap(CaughtInput, input.UserInputState)
+local Binding = {}
+Binding._actionMap = {}
+Binding._map = {}
+
+local function AddActionToKey(action, key)
+	if not Binding._map[key] then
+		Binding._map[key] = {}
 	end
+	table.insert(Binding._map[key], action)
 end
 
-function BindMap.map(NewMap: MappingData) --// Overwrite all data
-	BindMap._map = NewMap
-end
-
-function BindMap.softMap(NewMap) --// Add those which do not exist
-	print(NewMap)
-	if BindMap._map == nil then
-		BindMap._map = NewMap
+local function RemoveActionFromKey(action, key)
+	local Ref = Binding._map[key]
+	if not Ref then
 		return
 	end
-	ForPairs(NewMap, function(input, callback)
-		if BindMap._map[input] == nil then
-			BindMap._map[input] = callback or nil
+	table.remove(Binding._map[key], table.find(Ref, action))
+end
+
+function Binding.BulkMapAction(ActionMap: { ["string"]: (state: Enum.UserInputState) -> () })
+	Binding._actionMap = ActionMap
+end
+
+function Binding.Map(action: string, input: Input)
+	AddActionToKey(action, input)
+end
+
+function Binding.RMap(action: string, input: Input)
+	RemoveActionFromKey(action, input)
+end
+
+function Binding.BulkRMap(action: string)
+	ForPairs(Binding._map, function(input, actions)
+		local Index = table.find(actions, action)
+		if Index then
+			table.remove(Binding._map[input], Index)
 		end
 	end)
 end
 
-function BindMap.hardMap(NewMap) --// Override those which exist already.
-	if BindMap._map == nil then
-		return
-	end
-	ForPairs(NewMap, function(input, callback)
-		if BindMap._map[input] ~= nil then
-			BindMap._map[input] = callback or nil
-		end
+function Binding.BulkMap(Map: { [Input]: string })
+	ForPairs(Map, function(input, action)
+		Binding.Map(action, input)
 	end)
 end
 
-function BindMap.crossMap(NewMap: MappingData) --// Will overwrite existing variables without removing entire data
-	BindMap.hardMap(NewMap)
-	BindMap.softMap(NewMap)
-end
-
-UIS.InputBegan:Connect(OnInput)
-UIS.InputEnded:Connect(OnInput)
-UIS.InputChanged:Connect(OnInput)
-
-return BindMap
+return Binding
